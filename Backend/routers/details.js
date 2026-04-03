051c914d6de1d2b6669218a7e2798d3b5c094697
Dharmiksarvaiya24 <dharmiksarvaiya07@gmail.com>
2026-04-03 13:51:12 +0530

    MVC

---
+++ Backend/routers/details.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const{
+        getDetails,
+        getDetailsById,
+        updatedetails,
+        deletedetails,
+        createdetails,
+    } = require("../controller/details");
+
+
+router.get("/", getDetails);
+router.get("/:id",getDetailsById);
+router.patch("/api/:id",updatedetails);
+router.delete("/api/:id",deletedetails);
+router.post("/api",createdetails);
+
+
+module.exports = router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,116 +1,25 @@
 const express = require('express');
-const mongoose = require('mongoose');
-
 const app = express();
+
 require('dotenv').config();
 
-// MongoDB schema , model and connection
+const datarouter = require('./routers/details');
+const {connectDB} = require('./connection');
+const {log} = require('./middelwares');
 
-const flightdetails = new mongoose.Schema({
-  id: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  time: {
-    type: String,
-    required: true
-  },
-  departure: {
-    type: String,
-    required: true
-  },
-  arrival: {
-    type: String,
-    required: true
-  },
-},
-    { timestamps : true}
-);
-
-const flightdata = mongoose.model('flightdata', flightdetails);
-
-mongoose.connect(process.env.MongoDB_URI)
-.then(() => {
-    console.log("Connected to MongoDB");})
-.catch((err) => {
-    console.log("Error connecting to MongoDB", err);
+//connection
+connectDB(process.env.MongoDB_URI).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.log(err);
 });
-
 
 //Middleware
-
 app.use(express.urlencoded({extended: false}));
-
-app.use((req,res,next) => {
-   
-    next();
-});
+app.use(log);
 
 // routes
-
-app.get("/api/details", async (req, res) => {
-    
-    const data = await flightdata.find({});
-    return res.json(data);
-});
-
-app.get("/details", async (req, res) => {
-    
-    const data = await flightdata.find({});
-    const html=
-    `<ul>${data.map(item => 
-    `<li>${item.id}
-    ${item.name}
-    ${item.time}-hrs
-    ${item.arrival}-Arrival
-    ${item.departure}-Departure</li>`
-    ).join('')}</ul>`;
-    res.send(html);
-});
-
-app.get("/api/details/:id",async(req,res) => {
-    const id = req.params.id;
-    const item = await flightdata.findOne({ id: id });
-    return res.json(item);
-});
-
-app.post("/api/details", async (req,res) => {
-        const body = req.body;
-        if(
-            !body ||
-            !body.id ||
-            !body.name ||
-            !body.time ||
-            !body.departure ||
-            !body.arrival
-        )
-        {
-                return res.status(400).json({message: "All fields are req..."});
-        }
-  const result = await flightdata.create({
-    id: body.id,
-    name: body.name,
-    time: body.time,
-    departure: body.departure,
-    arrival: body.arrival
-   });
-   console.log(result);
-   return res.status(201).json({message: "Success", result});
-});
-
-app.patch("/api/details/:id",(req,res) => {
-    return res.json({message: "End Point for Patch request"});
-});
-
-app.delete("/api/details/:id",(req,res) => {
-    return res.json({message: "End Point for Delete request"});
-});
-
+app.use(('/api/details'), datarouter);
 
 app.listen(8080, () => {
     console.log(`Server is running on port 8080`);
